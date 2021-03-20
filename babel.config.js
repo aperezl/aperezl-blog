961d43c3aa77826652b092e00423c8f2e1157b34
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-03-20 10:29:59 +0100

    fix babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = {
+  ignore: [/\/(build|node_modules)\//],
   presets: [
+    'next/babel',
     '@babel/preset-env',
     '@babel/preset-react'
   ],
+  plugins: [
+    ['@babel/plugin-transform-react-jsx', { 'runtime': 'automatic' }]
+  ]
 };
