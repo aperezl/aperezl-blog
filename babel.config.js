12a0b48509e32434162f20474f1474e602d1860a
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-03-20 11:47:13 +0100

    config tailwind fonts and colors

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,8 @@ module.exports = {
   ignore: [/\/(build|node_modules)\//],
   presets: [
     'next/babel',
-    '@babel/preset-env',
-    '@babel/preset-react'
+    // '@babel/preset-env',
+    // '@babel/preset-react'
   ],
   plugins: [
     ['@babel/plugin-transform-react-jsx', { 'runtime': 'automatic' }]
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <div className="container mx-auto">
+      <h1 className="text-4xl text-cw-grey-050 font-bold font-roboto">Wello World</h1>
+      <p className="font-work-sans text-cw-grey-050">This is the home page</p>
+    </div>
   )
 }
