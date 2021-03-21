90f8057ace9471148b46fe04b00600d96ae3bdfb
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-03-21 02:19:54 +0100

    start navbar

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com"></link>
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&family=Work+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"></link>
         </Head>
-        <body className="bg-cw-blue-600">
+        <body className="font-roboto text-cw-grey-700 border-t-4 border-cw-grey-800">
           <Main />
           <NextScript />
         </body>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
 import React from 'react'
 export default function Home() {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-4xl text-cw-grey-050 font-bold font-roboto">Wello World</h1>
-      <p className="font-work-sans text-cw-grey-050">This is the home page</p>
-    </div>
+    <>
+      <header className="container mx-auto max-w-6xl px-5">
+        <div className="flex justify-between h-12">
+          <a className="flex justify-between items-center" href="#">
+            <img src="avatar.png" alt="Avatar Logo" className="h-12" />
+            <span className="font-bold fond-work-sans text-2xl text-cw-blue-400">aperezl</span>
+          </a>
+          <ul className="flex items-center">
+            <li className="mr-10">
+              <a href="#">Home</a>
+            </li>
+            <li className="mr-10">
+              <a href="#">Travel</a>
+            </li>
+            <li className="mr-10">
+              <a href="#">Music</a>
+            </li>
+            <li className="mr-10">
+              <a href="#">Technology</a>
+            </li>
+            <li className="mr-10">
+              <a href="#">Business</a>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </>
   )
 }
