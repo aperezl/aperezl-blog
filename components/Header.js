b3997af3cd9ed73c0ab02cc6a9e2e61255d1ebd3
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-03-23 08:24:36 +0100

    add Link component to header

--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,32 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
     <header className="bg-cw-grey-900">
       <div className="container mx-auto max-w-6xl px-5 flex justify-between h-12 py-10">
-        <a className="flex justify-between items-center" href="#">
-          <img src="/avatar.png" alt="Avatar Logo" className="h-12 rounded-full" />
-          <span className="px-2 font-bold fond-work-sans text-2xl text-cw-grey-050">aperezl</span>
-        </a>
+        <Link Link as={`/`} href="/">
+          <div className="flex justify-between items-center cursor-pointer">
+            <img src="/avatar.png" alt="Avatar Logo" className="h-12 rounded-full" />
+            <span className="px-2 font-bold fond-work-sans text-2xl text-cw-grey-050">aperezl</span>
+          </div>
+        </Link>
         <ul className="flex items-center text-cw-grey-050">
-          <li className="mr-10">
-            <a className="hover:text-cw-grey-100 hover:underline" href="#">Home</a>
+          <li className="mr-10 hover:text-cw-grey-100 hover:underline">
+            <Link Link as={`/`} href="/">
+              Home
+            </Link>
+  
           </li>
           <li className="mr-10">
-            <a className="hover:text-cw-grey-100 hover:underline" href="#">Travel</a>
+            <Link Link as={`/`} href="/">
+              Tags
+            </Link>
           </li>
           <li className="mr-10">
-            <a className="hover:text-cw-grey-100 hover:underline" href="#">Music</a>
-          </li>
-          <li className="mr-10">
-            <a className="hover:text-cw-grey-100 hover:underline" href="#">Technology</a>
-          </li>
-          <li>
-            <a className="hover:text-cw-grey-100 hover:underline" href="#">Business</a>
+            <Link Link as={`/`} href="/">
+              Sobre mi
+            </Link>
           </li>
         </ul>
       </div>
