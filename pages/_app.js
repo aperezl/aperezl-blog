0012fcc48b46b8e0c2c9bebae85f61407ffaec4e
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-05-25 21:30:54 +0200

    config screen_view in google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,27 @@
 import { useEffect } from 'react'
 import { analytics } from '../utils/firebase'
+import { useRouter } from 'next/router'
 
 import '../styles/globals.css'
 import 'highlight.js/styles/darcula.css';
 import 'highlight.js/lib/languages/javascript';
 
 function MyApp({ Component, pageProps }) {
+  const routers = useRouter()
   useEffect(() => {
     console.log('init analytics')
     console.log(process.env.NEXT_PUBLIC_FB_MEASUREMENTID)
     // if (process.env.NODE_ENV === 'production') {
-      analytics();
+      const logEvent = (url) => {
+        analytics().setCurrentScreen(url)
+        analytics().logEvent('screen_view')
+      }
+      routers.events.on('routeChangeComplete', logEvent)
+      logEvent(window.location.pathname)
+      return () => {
+        routers.events.off('routeChangeComplete', logEvent)
+      }
+
     // }
   }, [])
   return <Component {...pageProps} />
